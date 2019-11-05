3aba8abc4a3284e1278740f94277d104aa61ab04
Vinita Sharma <49115602+Vini-S@users.noreply.github.com>
2019-11-05 10:17:04 +0000

    Update client.py

--- chatapp/client.py
+++ chatapp/client.py
@@ -3,7 +3,7 @@ import socket
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-serversocket.connect(("192.168.0.121", 1234))
+serversocket.connect(("192.168.0.108", 1234))
 
 full_msg = ''
 
@@ -19,21 +19,23 @@ while True:
 	full_msg += msg.decode('utf-8')
 print(full_msg)
 '''
+def validate_response():
+	msg = serversocket.recv(1024)
+	if msg[:3].decode('utf-8') != "200":
+		print("Connection closed by the server")
+		serversocket.close()
+		exit()
+	return msg[4:].decode('utf-8')
 
-msg = serversocket.recv(1024) # receing welcome message from server
-if msg.decode('utf-8') == '':
-	print("Invalid user")
-	exit()
-else:
-	print(msg.decode('utf-8'))
+print(validate_response()) # receving welcome message from server
 
-choice = serversocket.recv(1024) # receving choice from server
-print(choice.decode('utf-8'))
+ # receving choice from server
+print(validate_response())
 request = input("enter your choice: ")
 serversocket.send(bytes(request,'utf-8')) # sending choice to server
 
-result = serversocket.recv(1024) # receving result of requested choice by client
-print(result.decode('utf-8'))
+# receving result of requested choice by client
+print(validate_response())
 
 c_name = input("enter the client name:")
 serversocket.send(bytes(c_name, 'utf-8'))
