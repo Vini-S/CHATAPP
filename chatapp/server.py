b4dd8dc494e592ff8915c469bbda64468a7754d2
Vinita Sharma <49115602+Vini-S@users.noreply.github.com>
2019-11-05 01:12:18 -0500

    Update server.py

--- chatapp/server.py
+++ chatapp/server.py
@@ -2,12 +2,9 @@ import socket, time, MySQLdb, threading
 
 conn = MySQLdb.connect(host="localhost", user="root", database="chatapp")
 cursor = conn.cursor()
-#cursor.execute("SELECT * FROM user")
-#data = cursor.fetchall()
-#print(data)
 
 serversocket = socket.socket()
-
+print(socket.gethostbyname(socket.gethostname()))
 serversocket.bind(("192.168.0.121",1234))
 
 serversocket.listen(5)
@@ -23,39 +20,41 @@ class user:
 	def login(self):
 		querry = cursor.execute(f"SELECT * FROM user WHERE username='{self.username}' AND password='{self.password}'")
 		data = cursor.fetchone()
-		if  data != None and self.username == data[1]:
-			return True
+		if  data == None or self.username != data[1]:
+			return False
+		return True
 	def send_msg(self,msg):
 		self.c_socket.send(bytes(msg, "utf-8"))
 
+def get_c_response(c_socket):
+	res = c_socket.recv(1024)
+	return res.decode('utf-8')
 
 def c_thread():
 	c_socket, address = serversocket.accept()
-	cred = c_socket.recv(1024)
+	cred = get_c_response(c_socket)
 	cred = cred.split()
-	username = cred[0].decode('utf-8')
-	password = cred[1].decode('utf-8')
-
-	#clientsocket.send(bytes("WELCOME TO THE SERVER", "utf-8"))
+	username = cred[0]
+	password = cred[1]
 
 	c_users.append(user(username,password,c_socket,address))
 
 	if c_users[-1].login():
-		c_users[-1].send_msg("Welcome To the Server :)")
+		c_users[-1].send_msg("Welcome to the server :)")
 
 		print(f"connection from {c_users[-1].address} is successful ")
 
-		c_users[-1].send_msg("1)Online Users \n2)Quit\n")
+		c_users[-1].send_msg("1)Online Users\n2)Quit")
 
-		request = c_socket.recv(1024)
-		if int(request.decode('utf-8')) == 1:
+		request = get_c_response(c_socket)
+
+		if request == '1':
 			list_of_users = ", ".join([user.username for user in c_users])[::-1].replace(",", "& ",1)[::-1]
 			c_socket.send(bytes(list_of_users, 'utf-8'))
-			chat_user = c_socket.recv(1024)
+			chat_user = get_c_response(c_socket)
 			print(chat_user)
 	c_users.pop()
 	c_socket.close()
-
 
 while True:
 	t1 = threading.Thread(target=c_thread)
