941c0e15efccbca4704f2ebdf4c0016a2a1ab912
Vinita Sharma <49115602+Vini-S@users.noreply.github.com>
2019-11-06 14:17:49 +0000

    Update client.py

--- chatapp/client.py
+++ chatapp/client.py
@@ -19,13 +19,35 @@ while True:
 	full_msg += msg.decode('utf-8')
 print(full_msg)
 '''
+def chat(otheruser, msg):
+
+	while True:
+		print(msg)
+		message = input(f"enter the msg to chat with {otheruser} :")
+		#print(message)
+		#chat(c_name,message)
+		serversocket.send(bytes("chat:"+otheruser+':'+message, 'utf-8'))
+		response = validate_response()
+		print(response)
+		if response[0] != 'chat':
+			print("chat closed by server ")
+			serversocket.close()
+			exit()
+
+
 def validate_response():
 	msg = serversocket.recv(1024)
-	if msg[:3].decode('utf-8') != "200":
+	msg = msg.decode('utf-8').split(':')
+	if msg[0]=='chat':
+		chat(msg[1],msg[2])
+		print(msg)
+		#print(f'{msg[1]}:{msg[-1]}')
+		return msg
+	elif msg[0] != "200":
 		print("Connection closed by the server")
 		serversocket.close()
 		exit()
-	return msg[4:].decode('utf-8')
+	return msg[-1]
 
 print(validate_response()) # receving welcome message from server
 
@@ -39,3 +61,9 @@ print(validate_response())
 
 c_name = input("enter the client name:")
 serversocket.send(bytes(c_name, 'utf-8'))
+
+print(validate_response())
+
+x = input('enter the msg you want to send: ')
+serversocket.send(bytes(x, 'utf-8'))
+chat(c_name, x)
